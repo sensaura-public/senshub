466b19b27e729b4a69a036c3e8e8cbae182b4f88
Shane Gough <shane@sensaura.org>
2015-11-05 15:09:07 +1000

    Started fleshing out additional assemblies required.

---
+++ Utilities/FileSystem.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.IO;
+
+namespace Sensaura.Utilities
+{
+	/// <summary>
+	/// File access mode
+	/// </summary>
+	public enum FileAccess {
+		/// <summary>
+		/// Open an existing file for read only access.
+		/// </summary>
+		Read,
+
+		/// <summary>
+		/// Open the file for read and write operations.
+		/// </summary>
+		ReadAndWrite
+	}
+
+	/// <summary>
+	/// File and folder creation options.
+	/// </summary>
+	public enum CreationOptions {
+		/// <summary>
+		/// Raise an exception if the file already exists.
+		/// </summary>
+		FailIfExists,
+		/// <summary>
+		/// Open the existing file.
+		/// </summary>
+		OpenIfExists,
+		/// <summary>
+		/// Replace an existing file with a new (empty) one.
+		/// </summary>
+		ReplaceExisting
+	}
+
+	/// <summary>
+	/// Represents a folder in the application storage area
+	/// </summary>
+	public interface IFolder
+	{
+		/// <summary>
+		/// Create (or open) a new file in the folder.
+		/// </summary>
+		/// <param name="name">
+		/// The name of the file to open or create. The file name must not contain path
+		/// separators.
+		/// </param>
+		/// <param name="access">
+		/// The access mode to open the file with.
+		/// </param>
+		/// <param name="options">
+		/// Creation options.
+		/// </param>
+		/// <returns>A Stream instance to read or write to the file.</returns>
+		Stream CreateFile(string name, FileAccess access, CreationOptions options);
+
+		/// <summary>
+		/// Test if the given file exists in the folder.
+		/// </summary>
+		/// <param name="name">
+		/// The name of the file to open or create. The file name must not contain path
+		/// separators.
+		/// </param>
+		/// <returns>True if the file exists.</returns>
+		bool FileExists(string name);
+	}
+
+	/// <summary>
+	/// Represents the application storage area.
+	/// </summary>
+	public interface IFileSystem
+	{
+		/// <summary>
+		/// Create or open a new folder in the storage area.
+		/// </summary>
+		/// <param name="name">
+		/// The name of the folder to open or create. The name must not contain path
+		/// separators.
+		/// </param>
+		/// <param name="options">
+		/// Creation options
+		/// </param>
+		/// <returns>An IFolder instance representing the folder.</returns>
+		IFolder CreateFolder(string name, CreationOptions options);
+
+		/// <summary>
+		/// Determine if the named folder exists.
+		/// </summary>
+		/// <param name="name">
+		/// The name of the folder to open or create. The name must not contain path
+		/// separators.
+		/// </param>
+		/// <returns>True if the folder exists.</returns>
+		bool FolderExists(string name);
+	}
+
+}
