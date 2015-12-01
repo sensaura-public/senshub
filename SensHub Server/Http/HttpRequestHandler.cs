9cb908bef66d9eada2cb6ea30ff3c3e3c6cf6253
Shane Gough <shane@sensaura.org>
2015-12-01 14:48:11 +1000

    Basic static file serving now works.

--- SensHub Server/Http/HttpRequestHandler.cs
+++ SensHub Server/Http/HttpRequestHandler.cs
@@ -23,5 +23,32 @@ namespace SensHub.Server.Http
         /// object has been filled out already.
         /// </returns>
         public abstract string HandleRequest(string url, HttpListenerRequest request, HttpListenerResponse response);
+
+        /// <summary>
+        /// Helper method to generate HTTP 404 Not Found responses
+        /// </summary>
+        /// <param name="response"></param>
+        /// <param name="message"></param>
+        /// <returns></returns>
+        public string NotFound(HttpListenerResponse response, string message = null)
+        {
+            response.StatusCode = 404;
+            response.StatusDescription = "Item not found.";
+            return message;
+        }
+
+        /// <summary>
+        /// Helper method to generate HTTP 500 Server Error responses
+        /// </summary>
+        /// <param name="response"></param>
+        /// <param name="message"></param>
+        /// <returns></returns>
+        public string ServerError(HttpListenerResponse response, string message = null)
+        {
+            response.StatusCode = 500;
+            response.StatusDescription = "Internal server error.";
+            return message;
+        }
+
     }
 }
--- SensHub Server/Http/StaticFileHandler.cs
+++ SensHub Server/Http/StaticFileHandler.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -18,7 +19,31 @@ namespace SensHub.Server.Http
 
         public override string HandleRequest(string url, HttpListenerRequest request, HttpListenerResponse response)
         {
-            return url;
+            // Convert the URI into a full file system path
+            string path = Path.GetFullPath(Path.Combine(m_path, url));
+            if (path.Length < m_path.Length)
+            {
+                // Trying to do something tricky with indirect paths.
+                return NotFound(response);
+            }
+            // Is it a directory ?
+            if (Directory.Exists(path))
+            {
+                // Add a 'index.html' to it
+                path = Path.Combine(path, "index.html");
+            }
+            // Does the file exist ?
+            if (!File.Exists(path))
+                return NotFound(response);
+            // Set up the response
+            FileInfo info = new FileInfo(path);
+            response.ContentLength64 = info.Length;
+            response.ContentType = MimeType.FromExtension(path);
+            Stream input = File.Open(path, FileMode.Open);
+            input.CopyTo(response.OutputStream);
+            input.Close();
+            // No additional content
+            return null;
         }
     }
 }
